3e6ba775db6e4a95c55a13cab93d1b8072cf79f5
bhatiamunal8987 <bhatiamunal8987@gmail.com>
2021-01-10 00:14:11 +0530

    first commit

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { ApplicationModule } from '@angular/core';
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic"; 
+import { AppModule} from './app/app.module'
+
+platformBrowserDynamic().bootstrapModule(AppModule );
